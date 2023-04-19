c3e49befa7d20105523a31ca8868b307ab33e68f
Mulong Luo <ml2558@cornell.edu>
2023-04-18 21:20:22 -0400

    update

--- src/cache_guessing_game_env_impl.py
+++ src/cache_guessing_game_env_impl.py
@@ -531,35 +531,6 @@ class CacheGuessingGameEnv(gym.Env):
   def calc_correct_rate(self):
     return self.guess_buffer.count(True) / len(self.guess_buffer)
 
-  '''
-  evluate the correctness of an action sequence (action+ latency) 
-  action_buffer: list [(action, latency)]
-  '''
-  def calc_correct_seq(self, action_buffer):
-    last_action, _ = action_buffer[-1]
-    last_action = self.parse_action(last_action)
-    #print(last_action)
-    guess_addr = last_action[4]
-    #print(guess_addr)
-    self.reset(victim_addr = guess_addr)
-    self.total_guess = 0
-    self.correct_guess = 0
-    while self.total_guess < 20:
-      self.reset(victim_addr)
-      for i in range(0, len(action_buffer)):
-        p = action_buffer[i]
-        state, _, _, _ = self.step(p[0])
-        latency = state[0]
-        if latency != p[1]:
-          break
-      if i < len(action_buffer) - 1:
-        continue
-      else:
-        self.total_guess += 1
-        if guess_addr == self.victim_address:
-          self.correct_guess += 1
-    return self.correct_guess / self.total_guess
-
   def set_victim(self, victim_address=-1):
     self.victim_address = victim_address
 
@@ -587,19 +558,6 @@ class CacheGuessingGameEnv(gym.Env):
       self.vprint("victim address (hex) " + hex(self.victim_address))
     else:
       self.vprint("victim has empty access")
-
-  '''
-  use to render the result
-  not implemented
-  '''
-  def render(self, mode='human'):
-    return 
-
-  '''
-  not implememented  
-  '''
-  def close(self):
-    return
 
   '''
   use a given seed to randomize the cache
